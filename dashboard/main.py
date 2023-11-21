18491d576ab22543a3c1f519fbf65eac52df15fe
Romina Capurro <romcapurro@gmail.com>
2023-11-21 16:06:06 -0300

    imagen url

--- dashboard/main.py
+++ dashboard/main.py
@@ -12,7 +12,7 @@ matplotlib.rc('font', **font)
 st.title('Polo Tecnológico Córdoba')
 
 # Mostrar una imagen
-st.image('fabio-oyXis2kALVg-unsplash.jpg', use_column_width=True)
+st.image('https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', use_column_width=True)
 
 
 def new_line():
